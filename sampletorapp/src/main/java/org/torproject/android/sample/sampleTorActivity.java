368a0ab1700a060f16820131e1376c9d8c97bef7
n8fr8 <nathan@guardianproject.info>
2019-03-22 12:03:21 -0400

    update sample to work with new installer API

--- sampletorapp/src/main/java/org/torproject/android/sample/sampleTorActivity.java
+++ sampletorapp/src/main/java/org/torproject/android/sample/sampleTorActivity.java
@@ -1,16 +1,20 @@
 package org.torproject.android.sample;
 
+import android.app.Application;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.jrummyapps.android.shell.CommandResult;
+import com.jrummyapps.android.shell.Shell;
+
 import org.torproject.android.binary.TorResourceInstaller;
+import org.torproject.android.binary.TorServiceConstants;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.TimeoutException;
-
 
 public class sampleTorActivity extends AppCompatActivity {
 
@@ -20,7 +24,10 @@ public class sampleTorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sample_tor);
 
         try {
-            File fileTorBin = new TorResourceInstaller(this,getFilesDir()).installResources();
+            TorResourceInstaller torResourceInstaller = new TorResourceInstaller(this,getFilesDir());
+
+            File fileTorBin = torResourceInstaller.installResources();
+            File fileTorRc = torResourceInstaller.getTorrcFile();
 
             boolean success = fileTorBin != null && fileTorBin.canExecute();
 
@@ -28,17 +35,95 @@ public class sampleTorActivity extends AppCompatActivity {
 
             ((TextView)findViewById(R.id.lblStatus)).setText(message);
 
+            if (success)
+            {
+                runTorShellCmd (fileTorBin, fileTorRc);
+            }
+
+
         } catch (IOException e) {
             e.printStackTrace();
-            ((TextView)findViewById(R.id.lblStatus)).setText(e.getMessage());
+            logNotice(e.getMessage());
 
-        } catch (TimeoutException e) {
+        } catch (Exception e) {
             e.printStackTrace();
+            logNotice(e.getMessage());
         }
+    }
+
+    public void logNotice (String notice)
+    {
+        ((TextView)findViewById(R.id.lblStatus)).setText(notice);
+    }
+
+    public void logNotice (String notice, Exception e)
+    {
+        ((TextView)findViewById(R.id.lblStatus)).setText(notice);
+        Log.e("SampleTor","error occurred",e);
     }
 
     public void doTorThings ()
     {
         //please see this project: https://github.com/thaliproject/Tor_Onion_Proxy_Library/
+    }
+
+    private boolean runTorShellCmd(File fileTor, File fileTorrc) throws Exception
+    {
+        File appCacheHome = getDir(SampleTorServiceConstants.DIRECTORY_TOR_DATA,Application.MODE_PRIVATE);
+
+        boolean result = true;
+        if (!fileTorrc.exists())
+        {
+            logNotice("torrc not installed: " + fileTorrc.getCanonicalPath());
+            return false;
+        }
+
+        String torCmdString = fileTor.getCanonicalPath()
+                + " DataDirectory " + appCacheHome.getCanonicalPath()
+                + " --defaults-torrc " + fileTorrc;
+
+        int exitCode = -1;
+
+        try {
+            exitCode = exec(torCmdString + " --verify-config", true);
+        }
+        catch (Exception e)
+        {
+            logNotice("Tor configuration did not verify: " + e.getMessage(),e);
+            return false;
+        }
+
+        try {
+            exitCode = exec(torCmdString, true);
+        }
+        catch (Exception e)
+        {
+            logNotice("Tor was unable to start: " + e.getMessage(),e);
+            return false;
+        }
+
+        if (exitCode != 0)
+        {
+            logNotice("Tor did not start. Exit:" + exitCode);
+            return false;
+        }
+
+
+        return result;
+    }
+
+
+    private int exec (String cmd, boolean wait) throws Exception
+    {
+        CommandResult shellResult = Shell.run(cmd);
+
+
+      //  debug("CMD: " + cmd + "; SUCCESS=" + shellResult.isSuccessful());
+
+        if (!shellResult.isSuccessful()) {
+            throw new Exception("Error: " + shellResult.exitCode + " ERR=" + shellResult.getStderr() + " OUT=" + shellResult.getStdout());
+        }
+
+        return shellResult.exitCode;
     }
 }
