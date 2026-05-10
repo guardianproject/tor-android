2440f74ac7abbb06b8f69f2de15fdabbc00c1ac4
bitmold <dsnake@protonmail.com>
2026-05-10 19:56:35 -0400

    remove commented out/unused lint ignore statement

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,6 @@
 // Top-level build file where you can add configuration options common to all subprojects
-
 val versionCode = 49080
 val versionName = "0.4.9.8"
-
-//@file:Suppress("PropertyName")
-
 buildscript {
     repositories {
         mavenCentral()
